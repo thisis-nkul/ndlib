3ec4bb1c6404af435accb9429dbd610c7a31312a
Nakul Sharma <nakul120.ns@gmail.com>
2020-11-24 15:18:08 +0530

    added CrossEntropy

--- ndlib/nn/loss/CrossEntropy.py
+++ ndlib/nn/loss/CrossEntropy.py
@@ -1,3 +1,14 @@
-class CrossEntropyLoss:
-    def __init__(self):
-        pass
+import numpy as np
+import ndlib
+
+class CrossEntropyLoss(ndlib.Loss):
+
+    def __init__(self, Y_hat, Y, batch_size):
+        super().__init__(Y_hat, Y, batch_size)
+
+    def compute_cost(self):
+        self.cost = (1/self.m)*np.sum(-self.Y*np.log(self.Y_hat))
+
+    def backward(self):
+        return self.Y_hat - self.Y
+        #returns dZ and not dA
